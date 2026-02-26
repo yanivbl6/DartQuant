0aa850a7275f63ad0849f062aadf0061c3a63738
Yaniv Blumenfeld <yanivblm6@gmail.com>
2026-02-26 15:46:59 +0000

    Update experiment script to correct group IDs for runs and maintain static activation quantization

--- fake_quant/Script/experiments.sh
+++ fake_quant/Script/experiments.sh
@@ -8,14 +8,15 @@ export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7
 
 
 
-./Script/dart_gptq_wxaykvz.sh full -g 4 "$@" > /tmp/full_results.out 2> /tmp/full_results.err &
-./Script/dart_gptq_wxaykvz.sh baseline -g 5 -w 4 -a 8 -k 4 -G 128 --sym "$@" > /tmp/baseline_results.out 2> /tmp/baseline_results.err &
-./Script/dart_gptq_wxaykvz.sh quarot -g 6 -w 4 -a 8 -k 4 -G 128 --sym "$@" > /tmp/quarot_results.out 2> /tmp/quarot_results.err &
-./Script/dart_gptq_wxaykvz.sh dart -g 7 -w 4 -a 8 -k 4 -G 128 --sym "$@" > /tmp/dart_results.out 2> /tmp/dart_results.err &
+./Script/dart_gptq_wxaykvz.sh full -g 1 "$@" > /tmp/full_results.out 2> /tmp/full_results.err &
+./Script/dart_gptq_wxaykvz.sh baseline -g 2 -w 4 -a 8 -k 4 -G 128 --sym "$@" > /tmp/baseline_results.out 2> /tmp/baseline_results.err &
+./Script/dart_gptq_wxaykvz.sh quarot -g 3 -w 4 -a 8 -k 4 -G 128 --sym "$@" > /tmp/quarot_results.out 2> /tmp/quarot_results.err &
+./Script/dart_gptq_wxaykvz.sh dart -g 4 -w 4 -a 8 -k 4 -G 128 --sym "$@" > /tmp/dart_results.out 2> /tmp/dart_results.err &
 
-./Script/dart_gptq_wxaykvz.sh baseline -g 5 -w 4 -a 8 -k 4 -G 128 --sym --static-act "$@" > /tmp/baseline_results.out 2> /tmp/baseline_results.err &
-./Script/dart_gptq_wxaykvz.sh quarot -g 6 -w 4 -a 8 -k 4 -G 128 --sym --static-act "$@" > /tmp/quarot_results.out 2> /tmp/quarot_results.err &
-./Script/dart_gptq_wxaykvz.sh dart -g 7 -w 4 -a 8 -k 4 -G 128 --sym --static-act "$@" > /tmp/dart_results.out 2> /tmp/dart_results.err &
+# --- Static activation quantization runs (reuse GPTQ checkpoints from above) ---
+./Script/dart_gptq_wxaykvz.sh baseline -g 5 -w 4 -a 8 -k 4 -G 128 --sym --static-act "$@" > /tmp/baseline_static_results.out 2> /tmp/baseline_static_results.err &
+./Script/dart_gptq_wxaykvz.sh quarot -g 6 -w 4 -a 8 -k 4 -G 128 --sym --static-act "$@" > /tmp/quarot_static_results.out 2> /tmp/quarot_static_results.err &
+./Script/dart_gptq_wxaykvz.sh dart -g 7 -w 4 -a 8 -k 4 -G 128 --sym --static-act "$@" > /tmp/dart_static_results.out 2> /tmp/dart_static_results.err &
 
 
 
